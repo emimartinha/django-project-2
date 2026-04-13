bde28315ff63f7ae7dfc044dce7134fd50dbbf04
EmilianoMartinha <emimartinha@gmail.com>
2026-04-13 19:53:39 -0300

    Fix database configuration for deployment

--- config/settings.py
+++ config/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://postgres:postgres@localhost:5432/django_crud', conn_max_age=600)}
-
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'}}
+        
+        
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
