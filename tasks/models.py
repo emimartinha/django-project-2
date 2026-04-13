3662ba0e6ad4953e0d6612b4c9105999007178d5
EmilianoMartinha <emimartinha@gmail.com>
2026-04-13 18:57:28 -0300

    Initial commit - Django CRUD project

---
+++ tasks/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    datecompleted=models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
